25479edd1f668bd85957e8e1ae0c273ada87b48f
sunset-cf <cfische5@gmail.com>
2024-10-30 20:10:05 -0700

    Fixed incorrect usage of IsBlackKey and IsWhiteKey

--- Assets/Script/Gameplay/Player/ProKeysPlayer.cs
+++ Assets/Script/Gameplay/Player/ProKeysPlayer.cs
@@ -432,7 +432,7 @@ namespace YARG.Gameplay.Player
                 int leftKey = Math.Min(note.Key, note.NextNote.Key);
                 int rightKey = Math.Max(note.Key, note.NextNote.Key);
                 
-                bool keysAreSameType = ProKeysUtilities.IsBlackKey(leftKey) == ProKeysUtilities.IsBlackKey(rightKey);
+                bool keysAreSameType = ProKeysUtilities.IsBlackKey(leftKey % 12) == ProKeysUtilities.IsBlackKey(rightKey % 12);
                 
                 if (!keysAreSameType && rightKey - leftKey == 1)
                 {
@@ -451,7 +451,7 @@ namespace YARG.Gameplay.Player
                 }
             }
             
-            if (ProKeysUtilities.IsWhiteKey(note.Key))
+            if (ProKeysUtilities.IsWhiteKey(note.Key % 12))
             {
                 // White notes are slightly wider than the lane
                 lane.MultiplyScale(1.25f);
